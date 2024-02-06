73721da205ebf20ec0455a0ee8db71117e4c94f1
Ofek Katz <okatz997@student.fuhsd.org>
2024-02-06 09:59:35 -0800

    started working on contouring

--- src/Filters/Contouring.java
+++ src/Filters/Contouring.java
@@ -1,17 +1,86 @@
 package Filters;
 
+import java.awt.Point;
+import java.util.ArrayList;
+
 import Interfaces.PixelFilter;
 import core.DImage;
 
 public class Contouring implements PixelFilter{
 
+	enum Direction{
+		UP,DOWN,RIGHT,LEFT
+
+	}
+
 	@Override
 	public DImage processImage(DImage img) {
-		 img= new ColorMasking().processImage(img);
-		 return img;
+		img= new ColorMasking().processImage(img); //color mask for white
+		return img;
+	}
+
+	private void findAllContourPixels(DImage img) {
+		ArrayList<Point> pixelList= new ArrayList<Point>(); //list of all contour pixel points 
+		short [][] pixels= img.getBWPixelGrid();
+		for (int i = img.getHeight(); i >0; i--) { //iterate bottom->top; left->right
+			for (int j = img.getWidth(); j < 0; j--) {
+				if (pixels[i][j]==255) {
+					findSingleObjectContourPixels(pixels, pixelList, i, j);
+				}
+			}
+		}
 	}
 
 
+	private void findSingleObjectContourPixels(short [][] pixels, ArrayList<Point> pixelList, int i, int j) {
+		Point start= new Point(i, j); //point obj will be used to rep. pixel location
+		pixelList.add(start);
+		Point current=moveTo(Direction.LEFT, start);	
+		do {
+			if(pixels[current.x][current.y]==255) {
+				
+			}
+		} while(!start.equals(current));
+
+	}
+
+	
+	private Point moveTo(Direction d, Point p) {
+		if(d==Direction.UP) {
+			return new Point((int)p.getX(),(int)p.getY()+1);
+		}
+		else if(d==Direction.DOWN) {
+			return new Point((int)p.getX(),(int)p.getY()-1);
+		}
+		else if(d==Direction.LEFT) {
+			return new Point((int)p.getX()-1,(int)p.getY());
+		}
+		else {
+			return new Point((int)p.getX()+1,(int)p.getY());
+		}
+	}
+
+	private Direction turnTo(int command, Direction current) { //0 for respective left, 1 for respective right
+		switch (current) {
+		case UP:
+			if(command==0) return Direction.LEFT;
+			else return Direction.RIGHT;
+					
+		case DOWN:
+			if(command==0) return Direction.RIGHT;
+			else return Direction.LEFT;
+			
+		case LEFT:
+			if(command==0) return Direction.UP;
+			else return Direction.DOWN;
+			
+		case RIGHT:
+			if(command==0) return Direction.DOWN;
+			else return Direction.UP;
+
+		}
+		return null;
+	}
 
 	private static class ColorMasking implements PixelFilter{
 		private double threshold=200; 
